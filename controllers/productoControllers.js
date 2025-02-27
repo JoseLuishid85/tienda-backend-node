a6eb9c1c15ee4fe046f768fd9f958a63ff43350b
Jose Luis Hidalgo <joseluishid01@gmail.com>
2025-02-27 15:30:32 -0400

    Productos

--- controllers/productoControllers.js
+++ controllers/productoControllers.js
@@ -1,5 +1,11 @@
-const Producto = require('../models/Producto.js')
 const slugify = require('slugify');
+const { Op } = require('sequelize');
+const path = require('path');
+const fs = require('fs');
+
+const Categoria = require('../models/Categoria.js');
+const Producto = require('../models/Producto.js')
+const SubCategoria = require('../models/SubCategoria.js');
 
 const registro_producto = async (req, res) => {
 
@@ -44,6 +50,186 @@ const registro_producto = async (req, res) => {
 
 }
 
+const getProductoAdmin = async (req, res) => {
+
+    if (!req.usuario) {
+        res.status(500).json({
+            data: undefined,
+            msg: 'Error Token',
+            dd: req.usuario
+        });
+        return
+    }
+
+    let filtro = req.params['filtro'];
+    let whereCondition = {};
+
+    if (filtro) {
+        whereCondition = {
+            [Op.or]: [
+                { titulo: { [Op.like]: `%${filtro}%` } },
+            ]
+        };
+    }
+
+    let producto = await Producto.findAll({
+        where: whereCondition,
+        include:[
+            {
+                model: Categoria,
+                as: 'categoria', 
+            },
+            {
+                model: SubCategoria,
+                as: 'subCategoria', 
+            }
+        ],
+        order: [['createdAt', 'DESC']]
+    });
+
+    res.status(200).json(producto);
+
+}
+
+const obtenerProductoAdmin = async (req, res) => {
+
+    if (!req.usuario) {
+        res.status(500).json({
+            data: undefined,
+            msg: 'Error Token',
+            dd: req.usuario
+        });
+        return
+    }
+
+    let id = req.params['id'];
+
+    try {
+        let producto = await Producto.findOne({
+            where: {
+                id: id
+            },
+            include:[
+                {
+                    model: Categoria,
+                    as: 'categoria', 
+                },
+                {
+                    model: SubCategoria,
+                    as: 'subCategoria', 
+                }
+            ],
+        });
+
+        if (!producto) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El Producto no existe en la base de dato',
+            });
+        }
+
+        res.status(200).send(producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            message: 'Error al procesar datos',
+        })
+    }
+}
+/*
+const actualizarProductoAdmin = async (req, res) => {
+    if (!req.usuario) {
+        return res.status(500).json({
+            data: undefined,
+            msg: 'Error Token',
+            dd: req.usuario
+        });
+    }
+
+    let data = req.body;
+    let id = parseInt(req.params['id']);
+
+    const existingProduct = await Producto.findOne({ where: { titulo: data.titulo } });
+    if (existingProduct && existingProduct.id !== id) {
+        return res.status(500).send({ data: undefined, msg: 'El título ya existe en la base de datos' });
+    }
+
+    try {
+        if (req.file) {
+            const img_path = req.file.path;
+            const str_img = img_path.split('\\');
+            const str_portada = str_img[str_img.length - 1];
+            data.portada = str_portada;
+        }
+
+        data.slug = slugify(data.titulo).toLowerCase();
+
+        const producto = await Producto.update(data, {
+            where: { id: id }
+        });
+
+        res.status(200).send({
+            data: producto
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ ok: false, data: undefined, msg: 'Error al procesar datos' });
+    }
+};*/
+
+const listaProductoActivoAdmin = async (req, res) => {
+
+    if (!req.usuario) {
+        res.status(500).json({
+            data: undefined,
+            msg: 'Error Token',
+            dd: req.usuario
+        });
+        return
+    }
+
+
+    let producto = await Producto.findAll({
+        where: {
+            estado: true
+        },
+        order: [['createdAt', 'DESC']]
+    });
+
+    res.status(200).json(producto);
+
+}
+
+const obtenerImageProducto = async (req, res) => {
+    const img = req.params['img'];
+
+    // Validar que el nombre de la imagen no contenga caracteres peligrosos
+    if (/\.\./g.test(img)) {
+        return res.status(400).send({ message: 'Nombre de archivo no válido' });
+    }
+
+    // Construir la ruta de la imagen
+    const imagePath = path.join(__dirname, '../uploads/productos', img);
+
+    try {
+        // Verificar si la imagen existe
+        await fs.promises.access(imagePath, fs.constants.F_OK);
+
+        // Si existe, enviar la imagen
+        res.status(200).sendFile(imagePath);
+    } catch (error) {
+        // Si no existe, enviar la imagen por defecto
+        const defaultImagePath = path.join(__dirname, '../uploads/default.jpg');
+        res.status(404).sendFile(defaultImagePath);
+    }
+};
+
 module.exports = {
-    registro_producto
+    registro_producto,
+    getProductoAdmin,
+    obtenerProductoAdmin,
+    obtenerImageProducto,
+    listaProductoActivoAdmin
 }
--- routes/productoRouter.js
+++ routes/productoRouter.js
@@ -2,7 +2,13 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const validarJWT = require('../middlewares/validar-token.js');
-const { registro_producto } = require('../controllers/productoControllers.js');
+const { 
+    registro_producto, 
+    getProductoAdmin, 
+    obtenerImageProducto, 
+    listaProductoActivoAdmin,
+    obtenerProductoAdmin, 
+} = require('../controllers/productoControllers.js');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -19,5 +25,11 @@ const upload = multer({ storage });
 const routes = express.Router();  
 
 routes.post('/', [validarJWT, upload.single('portada')], registro_producto);
+routes.get('/buscar/:filtro?', validarJWT, getProductoAdmin);
+routes.get('/:id', validarJWT, obtenerProductoAdmin);
+//routes.put('/:id', [validarJWT, upload.single('portada')], actualizarProductoAdmin);
+
+routes.get('/lista_producto_activo/', validarJWT, listaProductoActivoAdmin);
+routes.get('/obtener_image_producto/:img', obtenerImageProducto); 
 
 module.exports = routes;
