6fc4afe7388b648321bb353172afada5cdc912e5
Jose Luis Hidalgo <joseluishid01@gmail.com>
2025-11-24 12:37:35 -0400

    Prueba 2

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ sequelize.sync({ alter: false })
     })
     .catch(err => console.error('Error al sincronizar la BD:', err));
 
-app.use('/store/api/data/',  require('./routes/dataRouter.js'));
-/*
+//app.use('/store/api/data/',  require('./routes/dataRouter.js'));
+
 app.use('/store/api/login', require('./routes/authRouter.js'));
 app.use('/store/api/usuario', require('./routes/usuario.js'));
 app.use('/store/api/categoria', require('./routes/categoriaRouter.js'));
@@ -24,15 +24,14 @@ app.use('/store/api/producto', require('./routes/productoRouter.js'));
 app.use('/store/api/variedad', require('./routes/variedadRouter.js'));
 app.use('/store/api/proveedor', require('./routes/proveedorRouter.js'));
 app.use('/store/api/cliente', require('./routes/clienteRouter.js'));
-app.use('/store/api/direccion/',  require('./routes/direccionRouter.js'));
-app.use('/store/api/ingreso/', require('./routes/ingresoRouter.js'));
-app.use('/store/api/detalle_ingreso/', require('./routes/detalleIngresoRouter.js'));
-app.use('/store/api/publico/',  require('./routes/publicoRouter.js'));
-app.use('/store/api/customer/',  require('./routes/customerRouter.js'));
-app.use('/store/api/venta/',  require('./routes/ventaRouter.js'));
-app.use('/store/api/detalleventa/',  require('./routes/detalleventaRouter.js'));
-app.use('/store/api/banco/',  require('./routes/bancoRouter.js'));
-*/
+app.use('/store/api/direccion',  require('./routes/direccionRouter.js'));
+app.use('/store/api/ingreso', require('./routes/ingresoRouter.js'));
+app.use('/store/api/detalle_ingreso', require('./routes/detalleIngresoRouter.js'));
+app.use('/store/api/publico',  require('./routes/publicoRouter.js'));
+app.use('/store/api/customer',  require('./routes/customerRouter.js'));
+app.use('/store/api/venta',  require('./routes/ventaRouter.js'));
+app.use('/store/api/detalleventa',  require('./routes/detalleventaRouter.js'));
+app.use('/store/api/banco',  require('./routes/bancoRouter.js'));
 
 app.use(express.urlencoded({ extended: true }));
 
