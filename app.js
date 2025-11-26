8ae41fc322ee85e104e182cb0f5deec0c4fe886c
Jose Luis Hidalgo <joseluishid01@gmail.com>
2025-11-26 08:36:54 -0400

    Finalizacion de Prueba

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ sequelize.sync({ alter: false })
     })
     .catch(err => console.error('Error al sincronizar la BD:', err));
 
-app.use('/store/api/data/',  require('./routes/dataRouter.js'));
-/*
+//app.use('/store/api/data/',  require('./routes/dataRouter.js'));
+
 app.use('/store/api/login', require('./routes/authRouter.js'));
 app.use('/store/api/usuario', require('./routes/usuario.js'));
 app.use('/store/api/categoria', require('./routes/categoriaRouter.js'));
@@ -39,7 +39,7 @@ app.use('/store/api/venta',  require('./routes/ventaRouter.js'));
 app.use('/store/api/detalleventa',  require('./routes/detalleventaRouter.js'));
 app.use('/store/api/banco',  require('./routes/bancoRouter.js'));
 
-app.use(express.urlencoded({ extended: true }));*/
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 4000;
 
