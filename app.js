bfd6fda4e9e417227612264e2b4d887063d2ffa6
unknown <joseluishid01@gmail.com>
2026-06-03 15:13:13 -0400

    Activar endpoints

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ sequelize.sync({ alter: false })
   })
   .catch(err => console.error('Error al sincronizar la BD:', err));
 
-app.use('/valery/api/data/', require('./routes/dataRouter.js'));
-/*
+//app.use('/valery/api/data/', require('./routes/dataRouter.js'));
+
 app.use('/valery/api/login', require('./routes/authRouter.js'));
 app.use('/valery/api/usuario', require('./routes/usuario.js'));
 app.use('/valery/api/categoria', require('./routes/categoriaRouter.js'));
@@ -30,15 +30,15 @@ app.use('/valery/api/producto', require('./routes/productoRouter.js'));
 app.use('/valery/api/variedad', require('./routes/variedadRouter.js'));
 app.use('/valery/api/proveedor', require('./routes/proveedorRouter.js'));
 app.use('/valery/api/cliente', require('./routes/clienteRouter.js'));
-app.use('/valery/api/direccion',  require('./routes/direccionRouter.js'));
+app.use('/valery/api/direccion', require('./routes/direccionRouter.js'));
 app.use('/valery/api/ingreso', require('./routes/ingresoRouter.js'));
 app.use('/valery/api/detalle_ingreso', require('./routes/detalleIngresoRouter.js'));
-app.use('/valery/api/publico',  require('./routes/publicoRouter.js'));
-app.use('/valery/api/customer',  require('./routes/customerRouter.js'));
-app.use('/valery/api/venta',  require('./routes/ventaRouter.js'));
-app.use('/valery/api/detalleventa',  require('./routes/detalleventaRouter.js'));
-app.use('/valery/api/banco',  require('./routes/bancoRouter.js'));
-app.use('/valery/api/report',  require('./routes/reportRouter.js'));*/
+app.use('/valery/api/publico', require('./routes/publicoRouter.js'));
+app.use('/valery/api/customer', require('./routes/customerRouter.js'));
+app.use('/valery/api/venta', require('./routes/ventaRouter.js'));
+app.use('/valery/api/detalleventa', require('./routes/detalleventaRouter.js'));
+app.use('/valery/api/banco', require('./routes/bancoRouter.js'));
+app.use('/valery/api/report', require('./routes/reportRouter.js'));
 
 app.use(express.urlencoded({ extended: true }));
 
