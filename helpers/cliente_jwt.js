a87c410fddd9e5f2fea9c79c229b2c508321a3d5
unknown <joseluishid01@gmail.com>
2026-06-21 19:45:34 -0400

    Token

--- helpers/cliente_jwt.js
+++ helpers/cliente_jwt.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const generarClienteJWT = (userId) =>{
-    const token = jwt.sign({ id: userId},  process.env.JWT_SECRET_CLIENTE,  { expiresIn: '5d' } );
+const generarClienteJWT = (userId) => {
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET_CLIENTE, { expiresIn: '100d' });
 
     return token;
 }
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const generarJWT = (userId) =>{
-    const token = jwt.sign({ id: userId},  process.env.JWT_SECRET,  { expiresIn: '5d' } );
+const generarJWT = (userId) => {
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
     return token;
 }
